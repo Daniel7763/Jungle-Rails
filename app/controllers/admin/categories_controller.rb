d0d56f2862a83a6a58d8eec006eba4b210d69d79
Daniel Soares <danielsoares01@outlook.com>
2023-07-11 21:33:36 -0700

    new category function

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,4 +8,20 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+  
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
